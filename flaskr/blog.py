9c2fd6b68654961cbf09139c205e465edb6346ae
corneheijnen <corneheijnen@hellofresh.nl>
2023-09-22 13:09:05 +0200

    update

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -2,6 +2,7 @@ from flask import Blueprint, abort, flash, g, redirect, render_template, request
 
 from flaskr.auth import login_required
 from flaskr.db import get_db
+from sqlalchemy.sql import text
 
 blueprint = Blueprint('blog', __name__, url_prefix='/')
 
@@ -9,9 +10,9 @@ blueprint = Blueprint('blog', __name__, url_prefix='/')
 @blueprint.route('/')
 def index():
     db = get_db()
-    posts = db.execute('SELECT p.id, title, body, created, author_id, username'
-                       ' FROM post p JOIN user u ON p.author_id = u.id'
-                       ' ORDER BY created DESC').fetchall()
+    posts = db.execute(text("""SELECT p.id, title, body, created, author_id, username
+                       FROM post p JOIN user u ON p.author_id = u.id
+                       ORDER BY created DESC""")).fetchall()
     return render_template('blog/index.html', posts=posts)
 
 
@@ -31,9 +32,9 @@ def create():
 
         else:
             db = get_db()
-            db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                ' VALUES (?, ?, ?)', (title, body, g.user['id']))
+            db.execute(text(
+                """INSERT INTO post (title, body, author_id)
+                VALUES (:title, :body, :id)"""), parameters={"title": title, "body": body, "id": g.user[0]})
             db.commit()
             return redirect(url_for('blog.index'))
 
@@ -41,15 +42,18 @@ def create():
 
 
 def get_post(id, check_author=True):
-    post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?', (id, )).fetchone()
+    post = get_db().execute(text(
+        """SELECT p.id, title, body, created, author_id, username
+        FROM post p JOIN user u ON p.author_id = u.id
+        WHERE p.id = :id """), {'id': id}).fetchone()
 
     if post is None:
         abort(404, f"Post id {id} doesn't exist.")
 
-    if check_author and post['author_id'] != g.user['id']:
+    if check_author and post[-2] != g.user[0]:
+        print(post[1])
+        print(post)
+        print(g.user[0])
         abort(403)
 
     return post
@@ -72,8 +76,8 @@ def update(id):
             flash(error)
         else:
             db = get_db()
-            db.execute('UPDATE post SET title = ?, body = ?'
-                       ' WHERE id = ?', (title, body, id))
+            db.execute(text("""UPDATE post SET title = :title, body = :body
+                       WHERE id = :id"""), {'title': title, 'body': body, 'id': id})
             db.commit()
             return redirect(url_for('blog.index'))
 
@@ -85,6 +89,6 @@ def update(id):
 def delete(id):
     get_post(id)
     db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id, ))
+    db.execute(text('DELETE FROM post WHERE id = :id'), {'id': id})
     db.commit()
     return redirect(url_for('blog.index'))
